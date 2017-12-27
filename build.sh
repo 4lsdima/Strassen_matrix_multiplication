c2c10c9e4b24eb399e5a0833cff50351549fd910
lsdima <4lsdima@gmail.com>
2017-12-27 14:37:02 +0300

    modified build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
-exe="M"
+exe1="Simple"
+exe2="Gauss"
+exe3="Strassen"
 
-if [ -f $exe ];then
-	rm $exe
+if [ -f $exe1 ] || [ -f $exe2 ] || [ -f $exe3 ];then
+	rm -f $exe1
+	rm -f $exe2
+	rm -f $exe3
 fi
 
-g++ "main.cpp" -o $exe -std=c++11 -lboost_system -lboost_thread -DNDEBUG -O3 -Ofast
 
-if [ -f $exe ];then
+
+g++ simple_test.cpp -std=c++11 -o $exe1 -DNDEBUG -O3 -Ofast
+
+g++ gauss_test.cpp -std=c++11 -o $exe2 -DNDEBUG -O3 -Ofast
+
+g++ strassen_test.cpp -std=c++11 -o $exe3 -lboost_system -lboost_thread -DNDEBUG -O3 -Ofast
+
+if [ -f $exe1 ] && [ -f $exe2 ] && [ -f $exe3 ];then
   echo "Building complete!"
-  echo "object is:" $exe
   echo
 fi
